dd06c7e56e697c7302076c3a53b63e4ef301dbe9
Tomas Lin <tomas.lin@velocitylight.net>
2020-12-31 17:08:50 +0800

    增加 Bottom tabs

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -8,15 +8,47 @@ Navigation.registerComponent('Setting', () => SettingScreen);
 export const startMain = () => {
   Navigation.setRoot({
     root: {
-      stack: {
-        children: [
-          {
-            component: {
-              name: 'Home'
+        bottomTabs: {
+          id: 'BOTTOM_TABS_HOME',
+          children: [
+            {
+              stack: {
+                children: [{
+                  component: {
+                    name: 'Home',
+                    options: {
+                      visible: true,
+                      title: 'Home'
+                    }
+                  }
+                }],
+                options: {
+                  bottomTab: {
+                    text: 'Home',
+                  }
+                }
+              }
+            },
+            {
+              stack: {
+                children: [{
+                  component: {
+                    name: 'Setting',
+                    options: {
+                      visible: true,
+                      title: 'Setting'
+                    }
+                  }
+                }],
+                options: {
+                  bottomTab: {
+                    text: 'Setting',
+                  }
+                }
+              }
             }
-          }
-        ]
-      }
+          ]
+        },
     }
   });
 };
