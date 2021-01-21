fbc324c6d104c5f71317c7e96a3d6932ff45beea
Tomas Lin <thekingofthemountain@gmail.com>
2021-01-22 02:31:07 +0800

    add show Toast function

--- template/utils/navigationHelper.js
+++ template/utils/navigationHelper.js
@@ -1,9 +1,22 @@
 import {Navigation} from 'react-native-navigation';
+import { Toast } from "native-base";
 import {
   CLEAN_REGULARLY_COMPONENT,
   LOGIN_COMPONENT,
   REGISTER_COMPONENT,
 } from '~/constants/components';
+
+/**
+ * @text {string} options 
+ * @buttonText {string} options
+ * @duration {number} options
+ * @position {string} options enums: ["top", "bottom"]
+ * @type {string} options enums: ["warning", "danger", "success"]
+ * @style {object} options Toast's Style
+ * @show {function} Displays the Toast will fire
+ * @hide {function} Hides the Toast will fire
+ */
+export const showToast = (options) => Toast.show(options);
 
 export const showRegisterModal = () => {
   Navigation.showModal({
