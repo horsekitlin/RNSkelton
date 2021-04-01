af8d14e47f8b9b63d0caafa35cd0a15d0a4a6e14
Tomas Lin <thekingofthemountain@gmail.com>
2021-04-01 20:23:07 +0800

    udpate init_project.sh

--- lastest/init_project.sh
+++ lastest/init_project.sh
@@ -1,10 +1,23 @@
 PROJ_NAME=$1
 
+echo "initial react native project"
 npx react-native init $PROJ_NAME
 cd $PROJ_NAME
-cp -R ../../template/. ./
+
+echo "copy files"
+cp -R ../template ./src
 rm -f App.js
 
 chmod u+x ../add_package.sh && ../add_package.sh
 
 yarn ios
+
+echo "move files"
+mv ./* ..
+mv ./.* ..
+
+echo "clear skelton files"
+cd ..
+rm -rf ./lastest ./template ./README.md
+
+echo "initial project $PROJ_NAME done" ;
