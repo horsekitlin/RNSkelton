ca240caba81e07eac5470c2d646f5c221fe6f9d3
Tomas Lin <thekingofthemountain@gmail.com>
2022-01-19 10:44:45 +0800

    update

--- lastest/nativeBase/add_packages.sh
+++ lastest/nativeBase/add_packages.sh
@@ -4,19 +4,19 @@ echo "Add redux..."
 yarn add redux react-redux redux-saga redux-persist
 
 echo "Add router..."
-yarn add react-native-navigation react-native-navigation-hooks
+yarn add react-native-navigation
 
 echo "Add UI element..."
-yarn add native-base@3.2.2 react-native-vector-icons
+yarn add react-native-elements react-native-vector-icons react-native-toast-message
 
 echo "Add Notification services..."
 yarn add @react-native-firebase/app @react-native-firebase/messaging 
 
 echo "Database..."
-yarn add @react-native-async-storage/async-storage redux-persist
+yarn add @react-native-async-storage/async-storage
 
 echo "Packages..."
-yarn add flux-constants yup color react-native-safe-area-context react-native-svg react-native-vector-icons react-native-modal-overlay react-native-config
+yarn add axios flux-constants yup color react-native-safe-area-context react-native-svg react-native-vector-icons react-native-super-grid react-native-config 
 
 
 echo "Dev Packages..."
