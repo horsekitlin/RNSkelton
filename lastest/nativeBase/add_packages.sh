ae8ef32a29bc0771aeaf0b0bc9b145d5563006dc
帕斯昂 <passonTomas@linweiqindeMacBook-Pro.local>
2022-06-09 18:15:42 +0800

    update add_package.sh

--- lastest/nativeBase/add_packages.sh
+++ lastest/nativeBase/add_packages.sh
@@ -4,7 +4,7 @@ echo "Add redux..."
 yarn add redux react-redux redux-saga redux-persist
 
 echo "Add router..."
-yarn add react-native-navigation
+yarn add @react-navigation/native-stack @react-navigation/native react-native-screens react-native-safe-area-context react-native-gesture-handler@2.1.1
 
 echo "Add UI element..."
 yarn add react-native-elements react-native-vector-icons react-native-toast-message
@@ -16,12 +16,9 @@ echo "Database..."
 yarn add @react-native-async-storage/async-storage
 
 echo "Packages..."
-yarn add axios flux-constants yup color react-native-safe-area-context react-native-svg react-native-vector-icons react-native-super-grid react-native-config 
+yarn add axios flux-constants yup react-native-safe-area-context react-native-vector-icons react-native-config 
 
 
 echo "Dev Packages..."
-yarn add -D babel-plugin-root-import redux-flipper react-native-flipper
-
-echo "router setting..."
-npx rnn-link
+yarn add -D babel-plugin-module-resolver redux-flipper react-native-flipper
 
